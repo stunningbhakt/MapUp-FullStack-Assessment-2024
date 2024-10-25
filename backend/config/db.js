936b1ba23c382161ab7c1609b08ee287842cb252
Adarsh saxena <adarshs@vconstruct.in>
2024-10-26 00:45:58 +0530

    Setting up the DB connection

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,1 +1,17 @@
 // config/db.js content goes here.
+const mongoose = require('mongoose');
+
+const connectDB = async () => {
+    try {
+        await mongoose.connect(process.env.DB_URI, {
+            useNewUrlParser: true,
+            useUnifiedTopology: true,
+        });
+        console.log('MongoDB connected');
+    } catch (error) {
+        console.error('DB connection error', error);
+        process.exit(1);
+    }
+};
+
+module.exports = { connectDB };
