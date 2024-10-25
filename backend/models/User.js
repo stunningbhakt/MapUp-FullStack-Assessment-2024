08afd1d3670fa336e55cc2e7a14bb54297e8a1f2
Adarsh saxena <adarshs@vconstruct.in>
2024-10-26 00:46:50 +0530

    Setting up the user model

--- backend/models/User.js
+++ backend/models/User.js
@@ -1,19 +1,20 @@
 // models/User.js content goes here.
-
-
 const mongoose = require('mongoose');
+const bcrypt = require('bcrypt');
 
-const connectDB = async () => {
-    try {
-        await mongoose.connect(process.env.DB_URI, {
-            useNewUrlParser: true,
-            useUnifiedTopology: true,
-        });
-        console.log('MongoDB connected');
-    } catch (error) {
-        console.error('DB connection error', error);
-        process.exit(1);
-    }
-};
+const userSchema = new mongoose.Schema({
+    username: String,
+    email: String,
+    password: String,
+    role: { type: String, enum: ['admin', 'manager', 'user'], default: 'user' },
+});
 
-module.exports = { connectDB };
+userSchema.pre('save', async function (next) {
+    if (!this.isModified('password')) return next();
+    const salt = await bcrypt.genSalt(10);
+    this.password = await bcrypt.hash(this.password, salt);
+    next();
+});
+
+module.exports = mongoose.model('User', userSchema);
+
